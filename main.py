f134727a612c48b66dc357f23a57201f0373d58e
Charl3s-T4n <104561880+Charl3s-T4n@users.noreply.github.com>
2022-04-28 23:45:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-#Write your code below this line 👇
-
-choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
-if choice1 == "left":
+#Write your code below this line 👇                                                               # Adding of \n so user input cursor will be on next line
+               
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()      # Adding of \ infront so that ' will still be printed 
+if choice1 == "left":                                                                                      
   choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
   if choice2 == "wait":
     choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
